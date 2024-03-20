766e440faff30a2fd8793619411c6a7edb581eb2
Wanyi Zhu <wzhu36@my.bcit.ca>
2024-03-19 21:34:41 -0700

    Updated contact info

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -27,7 +27,7 @@ export const sendEmail = async (formData: FormData) => {
   try {
     data = await resend.emails.send({
       from: "Contact Form <onboarding@resend.dev>",
-      to: "0831zhuwanyi@gmail.com",
+      to: "wzhu36@my.bcit.ca",
       subject: "Message from contact form",
       reply_to: senderEmail,
       react: React.createElement(ContactFormEmail, {
